fd1adb5515ea78ebeed23e871cafcf3bfe3121ed
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 21:03:04 +0530

    Update string_2.c

--- string_2.c
+++ string_2.c
@@ -5,7 +5,7 @@ int main()
      char *st1 = "Good Morning!";
      char st2[20];
      strcpy(st2,st1);
-     printf("Aatif\n");
+     printf("Amit\n");
      printf("Value of st2 = %s", st2);
 return 0;
 }
