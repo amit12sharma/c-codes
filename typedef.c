6abeccf87df992b05ff28741bafb4958db0bbf24
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 21:17:06 +0530

    Update typedef.c

--- typedef.c
+++ typedef.c
@@ -4,9 +4,11 @@ int main()
 {
     // type def use in c.
     typedef int* intptr;
+    printf("Amit\n");
     intptr a,b,c;
     b = &a;
     c = &b;
+   
     printf("%d %d",a,b);
     return 0;
 }
