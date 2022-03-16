094371efd347e2ccaead420a8e715ef7ccb8d913
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 20:42:47 +0530

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -4,7 +4,7 @@ void main()
 {
     int a = 76;
     int *ptr = &a;
-    printf("Aatif\n");
+    printf("Amit\n");
     printf("Value ofa %d\n",a);
     printf("Address of a %x\n",ptr);
     printf("Address of a %p\n",ptr);
