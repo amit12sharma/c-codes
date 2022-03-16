9d1819626d108576d34395a87d53e71173103c8f
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 20:41:50 +0530

    Update pointarray.c

--- pointarray.c
+++ pointarray.c
@@ -4,6 +4,7 @@ int main( )
    int *p,i;
    int val[7] = { 11, 22, 33, 44, 55, 66, 77 } ;
    p = val;
+   printf("Amit\n");
    for ( int i = 0 ; i<7 ; i++ )
    {
       printf("val[%d]: value is %d\n",i,*(p+i));
