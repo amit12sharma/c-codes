82b9977960608a00eba692029447bbbfdbe4e3fb
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 20:24:40 +0530

    Update extern.c

--- extern.c
+++ extern.c
@@ -9,7 +9,7 @@ int main()
 {
     extern int s;
     int a,b;
-    printf("Aatif\n");
+    printf("Amit\n");
     printf("\nEnter the value of a:");
     scanf("%d",&a);
     printf("\nEnter the value of b:");
