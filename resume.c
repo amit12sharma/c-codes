d7441bff9c95789f1cfa79792c4b2f24cf44344f
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 20:58:31 +0530

    Update resume.c

--- resume.c
+++ resume.c
@@ -2,9 +2,9 @@
 #include<conio.h>
 void main()
 {
-	printf("\n NAME:- Aatif Khan.");
-	printf("\n ADDRESS:- House No 162,Basant colony,sector 13,Delhi");
+	printf("\n NAME:- Amit Sharma");
+	printf("\n ADDRESS:- H-543/37 onkar nagar,Tri Nagar,Delhi");
 	printf("\n EDUCATION:- Pursuing Bachelor of Computer Applications");
-	printf("\n MOBILE:- +91 8858XXXXXX \n\n");
+	printf("\n MOBILE:- +91 9990XXXXXX \n\n");
     getch();
 }
