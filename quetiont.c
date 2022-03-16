e8c54d641f45c747104274353c5f36fe4fd504df
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 20:55:37 +0530

    Update quetiont.c

--- quetiont.c
+++ quetiont.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int dividend, divisor, quotient, remainder;
-    printf("Aatif\n");
+    printf("Amit\n");
     printf("Enter dividend: ");
     scanf("%d", &dividend);
     printf("Enter divisor: ");
