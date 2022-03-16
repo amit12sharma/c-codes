4289f19b7cd9e4f8feeaba4c2a373c6937a669fe
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 20:46:31 +0530

    Update ptrptr.c

--- ptrptr.c
+++ ptrptr.c
@@ -5,6 +5,7 @@ int main()
     int a = 2, *p, **q;
     p=&a;
     q=&p;
+    printf("Amit\n");
     printf("Value of a=%d address of a = %u\n",a,&a);
     printf("Value of a=%d address of a = %u\n",p,&p);
     printf("Value of a=%d address of a = %u\n",*p,&p);
