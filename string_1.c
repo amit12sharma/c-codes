3acc60d577f82bec8dccd0f5c313e6475ece5da8
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-03-16 21:02:46 +0530

    Update string_1.c

--- string_1.c
+++ string_1.c
@@ -2,7 +2,7 @@
 #include<string.h>
 
 int main(){
-    char st[30] = "Aatif ", st2[40] = "Khan";
+    char st[30] = "Amit ", st2[40] = "Sharma";
     strcat(st,st2);
     printf("Concatenated string = %s", st);
 return 0;
